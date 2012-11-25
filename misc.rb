109423c0d3f8adc68aed08f38818d5eeed46e298
Kim Neunert <k9ert@gmx.de>
2012-11-25 15:46:50 +0100

    added some ec2 stuff for my convenience

--- misc.rb
+++ misc.rb
@@ -3,3 +3,31 @@ dep 'hello world', :someoneelse do
     print "Hello World!"
   }
 end
+
+dep 'ec2 run wheezy' do
+  requires ['ec2 ready']
+  meet {
+    shell "ec2-run-instances ami-4d20a724 -k KimNeunertAWS"
+  }
+end
+
+dep 'ec2 ready' do
+  requires ['ec2-run-instances.bin']
+  met? {
+    ENV["EC2_PRIVATE_KEY"] && ENV["EC2_CERT"]
+  }
+
+  meet {
+    puts "You should do this (and hopefully you have that available):"
+    puts ". ~/ec2-env"
+  }
+
+end
+
+
+dep 'ec2-run-instances.bin' do
+   installs 'ec2-api-tools'
+end
+  
+  
+ 
