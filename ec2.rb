adda25d09d220ff402225c31f3f2dcedd05388e5
Kim Neunert <k9ert@gmx.de>
2012-12-07 17:36:33 +0100

    refactored ec2 stuff

---
+++ ec2.rb
@@ -0,0 +1,36 @@
+dep 'ec2 sshin' do
+  requires ['ec2 oneup']
+  met? { false }
+  meet {
+    ip = shell("ec2-describe-instances | grep running | cut  -f4 ")
+    puts "ssh -i ~/.ssh/KimNeunertAWS.pem #{ip}"
+  }
+end
+
+dep 'ec2 oneup' do
+  requires ['ec2 ready']
+  met? {
+    shell("ec2-describe-instances | grep running | cut  -f4 | wc -l").match(/1/)
+  }
+  meet {
+    shell "ec2-run-instances ami-4d20a724 -k KimNeunertAWS"
+  }
+end
+
+dep 'ec2 ready' do
+  requires ['ec2-run-instances.bin']
+  met? {
+    ENV["EC2_PRIVATE_KEY"] && ENV["EC2_CERT"]
+  }
+
+  meet {
+    puts "You should do this (and hopefully you have that available):"
+    puts ". ~/bin/ec2-env"
+  }
+
+end
+
+
+dep 'ec2-run-instances.bin' do
+   installs 'ec2-api-tools'
+end
